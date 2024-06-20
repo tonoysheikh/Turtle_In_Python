ab3d3c8da4646946602c4c689ef8f936dac00f2e
Salman Johir Tonoy <165488699+tonoysheikh@users.noreply.github.com>
2024-06-20 23:29:20 +0600

    up

---
+++ square.py
@@ -0,0 +1,27 @@
+import turtle as t
+import random as rd
+
+wn = t.Screen()
+t.setup(800 , 500)
+t.bgcolor("black")
+t.pencolor("white")
+
+t.up()
+t.lt(180)
+t.fd(350)
+t.down()
+
+for i in range(1,6):
+    rdcolor = (rd.random() , rd.random() , rd.random())
+    t.fillcolor(rdcolor)
+    t.begin_fill()
+    for _ in range(1,5):
+        t.bk(100)
+        t.lt(90)
+    t.end_fill()
+    t.up()
+    t.bk(150)
+    t.down()
+   
+
+wn.exitonclick()
