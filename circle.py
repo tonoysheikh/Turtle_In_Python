493bd6f091bd06a2755794a99d2e9433f11f7151
Salman Johir Tonoy <165488699+tonoysheikh@users.noreply.github.com>
2024-06-20 23:46:56 +0600

    up

---
+++ circle.py
@@ -0,0 +1,22 @@
+import turtle as t 
+import random as rd
+
+wn = t.Screen()
+t.bgcolor("black")
+t.pencolor("black")
+t.width(2)
+t.up()
+t.goto(-150,0)
+t.down()
+for _ in range(1,11):
+    rdcolor = (rd.random() , rd.random(),rd.random())
+    t.fillcolor(rdcolor)
+    t.begin_fill()
+    t.circle(50)
+    t.end_fill()
+    t.up()
+    t.fd(30)
+    t.down()
+
+t.hideturtle()  
+wn.exitonclick()
